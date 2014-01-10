ae003288740d955042f0716a969063af81c043a2
dominicfarr <dominicfarr@gmail.com>
2014-01-10 16:37:56 +0000

    Added replication - badly

--- src/main/java/com/qmetric/shiro/cache/MemcachedShiroCacheManager.java
+++ src/main/java/com/qmetric/shiro/cache/MemcachedShiroCacheManager.java
@@ -1,54 +1,37 @@
 package com.qmetric.shiro.cache;
 
 import com.google.common.collect.Maps;
-import net.spy.memcached.MemcachedClient;
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.cache.CacheManager;
 import org.apache.shiro.util.Destroyable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unchecked")
-public class MemcachedShiroCacheManager implements org.apache.shiro.cache.CacheManager, Destroyable {
+public class MemcachedShiroCacheManager implements CacheManager, Destroyable {
 
     private Logger LOG = LoggerFactory.getLogger(MemcachedShiroCacheManager.class);
 
-    private final String endpoint;
+    private final Map<String, ShiroMemcached> clients = Maps.newConcurrentMap();
 
-    private final int port;
+    private List<String> serverList;
 
-    private final Map<String, Memcached> clients = Maps.newConcurrentMap();
-
-    public MemcachedShiroCacheManager(final String endpoint, final int port) {
-        this.endpoint = endpoint;
-        this.port = port;
+    public MemcachedShiroCacheManager(final String[] serverList) {
+        this.serverList = Arrays.asList(serverList);
     }
 
     @Override
     public Cache getCache(String name) throws CacheException {
         LOG.debug(String.format("MemcachedShiroCacheManager.getCache(%s)", name));
 
-        if (nameIsNotFound(name)) clients.put(name, new Memcached(createNewCacheInstance()));
+        if (nameIsNotFound(name)) clients.put(name, new ShiroMemcached(serverList));
 
         return clients.get(name);
-    }
-
-    private MemcachedClient createNewCacheInstance() {
-        try {
-            return new MemcachedClient(buildSocketConnection());
-        } catch (IOException e) {
-            throw new CacheException(e);
-        }
-    }
-
-    private InetSocketAddress buildSocketConnection() {
-        LOG.debug(String.format("endpoint: %s:%s", endpoint, port));
-
-        return new InetSocketAddress(endpoint, port);
     }
 
     private boolean nameIsNotFound(String name) {
